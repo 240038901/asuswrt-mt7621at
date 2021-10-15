70a258c8c5d1d11e847360ab89bb4ddff1f5c2fb
240038901 <58842348+240038901@users.noreply.github.com>
2021-10-15 09:47:52 +0800

    Update qmi_wwan.c

--- release/src-ra-5010/linux/linux-3.10.14.x/drivers/net/usb/qmi_wwan.c
+++ release/src-ra-5010/linux/linux-3.10.14.x/drivers/net/usb/qmi_wwan.c
@@ -584,6 +584,7 @@ static const struct usb_device_id products[] = {
 	{QMI_FIXED_INTF(0x2357, 0x0201, 4)},	/* TP-LINK HSUPA Modem MA180 */
 	{QMI_FIXED_INTF(0x1bc7, 0x1200, 5)},	/* Telit LE920 */
 	{QMI_FIXED_INTF(0x1e2d, 0x12d1, 4)},	/* Cinterion PLxx */
+	{QMI_FIXED_INTF(0x2020, 0x2033, 4)},    /* BM806U */
 
 	/* 4. Gobi 1000 devices */
 	{QMI_GOBI1K_DEVICE(0x05c6, 0x9212)},	/* Acer Gobi Modem Device */
